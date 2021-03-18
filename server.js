7f89d0597b906c85e09a2a285b1f69bd1b7408ff
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-03-18 01:56:49 +0100

    small fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/stock-super-market-management'));
+app.use(express.static(__dirname + '/dist/StockSuperMarketManagement'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/stock-super-market-management/index.html'));
+res.sendFile(path.join(__dirname+'/dist/StockSuperMarketManagement/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
