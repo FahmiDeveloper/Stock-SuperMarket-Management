2a02d7ea6ac787257835ad6220375a1d4c9208ad
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-03-18 22:36:29 +0100

    add firebase to project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBsRNqCCntneHgZRhwDOKTEh1YsJStpXvQ",
+    authDomain: "stockdb-22747.firebaseapp.com",
+    databaseURL: "https://stockdb-22747-default-rtdb.firebaseio.com",
+    projectId: "stockdb-22747",
+    storageBucket: "stockdb-22747.appspot.com",
+    messagingSenderId: "42554469994",
+    appId: "1:42554469994:web:3551c3850661b2267fe413",
+    measurementId: "G-S48YNMZWVD"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBsRNqCCntneHgZRhwDOKTEh1YsJStpXvQ",
+    authDomain: "stockdb-22747.firebaseapp.com",
+    databaseURL: "https://stockdb-22747-default-rtdb.firebaseio.com",
+    projectId: "stockdb-22747",
+    storageBucket: "stockdb-22747.appspot.com",
+    messagingSenderId: "42554469994",
+    appId: "1:42554469994:web:3551c3850661b2267fe413",
+    measurementId: "G-S48YNMZWVD"
+  }
 };
 
 /*
