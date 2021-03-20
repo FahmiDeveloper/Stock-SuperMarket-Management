7448304fae2896f41cfe5f3496f49bcce47e6ee0
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-03-20 23:27:45 +0100

    display-name-user-in-navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/auth.service';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
 
   isConnected:boolean;
+  user: firebase.User;
+  userName: string;
 
-  constructor(public authService: AuthService, private router: Router) {}
+  constructor(private afAuth: AngularFireAuth, public authService: AuthService, private router: Router) {
+    afAuth.authState.subscribe(user => {
+      this.user = user;
+      if(!this.user.displayName) {
+        this.getNameFromEmail(this.user.email);
+      }
+    })
+  }
 
   ngOnInit() {
     this.authService.isConnected.subscribe(res=>{
@@ -27,6 +39,10 @@ export class HeaderComponent implements OnInit {
     }, (error) => {
       console.log("Logout error", error);
     });
+  }
+
+  getNameFromEmail(email) {
+    this.userName = email.substring(0, email.lastIndexOf("@"));
   }
 
 }
