27fa5bd873460cf8e899856e94a1d6a5bd1ec04a
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-03-27 22:40:12 +0100

    add upload picture in employee form

--- src/app/employees/employee-form/employee-form.component.ts
+++ src/app/employees/employee-form/employee-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireUploadTask, AngularFireStorage } from '@angular/fire/storage';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { EmployeeService } from 'src/app/shared/services/employee.service';
 
 @Component({
@@ -8,13 +11,41 @@ import { EmployeeService } from 'src/app/shared/services/employee.service';
 })
 export class EmployeeFormComponent implements OnInit {
 
-  constructor(private employeeService: EmployeeService) { }
+  basePath = '/PicturesEmployees';
+  imageUrl = '';                      
+  task: AngularFireUploadTask;
+
+  progressValue: Observable<number>;
+
+  constructor(
+    private employeeService: EmployeeService,
+    private fireStorage: AngularFireStorage,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
 
   save(employee) {
     this.employeeService.create(employee);
+    this.router.navigate(['/employees']);
   }
+
+  async onFileChanged(event) {
+    const file = event.target.files[0];
+    if (file) {
+       const filePath = `${this.basePath}/${file.name}`;  // path at which image will be stored in the firebase storage
+       this.task =  this.fireStorage.upload(filePath, file);    // upload task
+ 
+       // this.progress = this.snapTask.percentageChanges();
+       this.progressValue = this.task.percentageChanges();
+ 
+       (await this.task).ref.getDownloadURL().then(url => {this.imageUrl = url; });  // <<< url is found here
+ 
+     } else {  
+       alert('No images selected');
+       this.imageUrl = '';
+      }
+   }
 
 }
