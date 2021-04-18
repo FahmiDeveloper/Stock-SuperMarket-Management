2bb02ce432c9047bf8c9bccfac19da8a3f1b15d6
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-04-18 13:29:08 +0100

    fix code structure in stock out

--- src/app/stock/stock-menu/stock-out/stock-out-form/stock-out-form.component.ts
+++ src/app/stock/stock-menu/stock-out/stock-out-form/stock-out-form.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
 import * as moment from 'moment';
 import { take } from 'rxjs/operators';
-import { StockOut } from 'src/app/shared/models/stock-out.model';
+import Swal from 'sweetalert2';
+
 import { ProductService } from 'src/app/shared/services/product.service';
 import { StockOutService } from 'src/app/shared/services/stock-out.service';
-import Swal from 'sweetalert2';
+
+import { StockOut } from 'src/app/shared/models/stock-out.model';
 
 @Component({
   selector: 'app-stock-out-form',
   templateUrl: './stock-out-form.component.html',
   styleUrls: ['./stock-out-form.component.scss']
 })
+
 export class StockOutFormComponent implements OnInit {
 
   stockOutId;
@@ -23,22 +27,27 @@ export class StockOutFormComponent implements OnInit {
     private router: Router, 
     private route: ActivatedRoute,
     private productService: ProductService
-    ) {
-    this.products$ = this.productService.getAll();
+  ) {}
 
-    this.stockOutId = this.route.snapshot.paramMap.get('id');
-        if (this.stockOutId) {
-          this.stockOutService.getStockOutId(this.stockOutId).valueChanges().pipe(take(1)).subscribe(stockOut => {
-          this.stockOut = stockOut;
-        });
-      } else {
-        this.stockOut.date = moment().format('YYYY-MM-DD');
-        this.stockOut.time = moment().format('HH:mm');
-      }
+  ngOnInit() {
+    this.getStockOutData();
+    this.loadListProducts();
   }
 
+  getStockOutData() {
+    this.stockOutId = this.route.snapshot.paramMap.get('id');
+    if (this.stockOutId) {
+      this.stockOutService.getStockOutId(this.stockOutId).valueChanges().pipe(take(1)).subscribe(stockOut => {
+      this.stockOut = stockOut;
+    });
+    } else {
+      this.stockOut.date = moment().format('YYYY-MM-DD');
+      this.stockOut.time = moment().format('HH:mm');
+    }
+  }
 
-  ngOnInit(): void {
+  loadListProducts() {
+    this.products$ = this.productService.getAll();
   }
 
   save(stockOut) {
@@ -64,5 +73,4 @@ export class StockOutFormComponent implements OnInit {
   cancel() {
     this.router.navigate(['/stock-out']);
   }
-
 }
