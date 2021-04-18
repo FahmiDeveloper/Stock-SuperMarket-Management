ef7074df56a88545e74625650a4b5b88699794d7
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-04-18 12:51:33 +0100

    fix code structure in products

--- src/app/products/product-form/product-form.component.ts
+++ src/app/products/product-form/product-form.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { ActivatedRoute, Router } from '@angular/router';
+
 import * as moment from 'moment';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
-import { Product } from 'src/app/shared/models/product.model';
+import Swal from 'sweetalert2';
+
 import { CategoryService } from 'src/app/shared/services/category.service';
 import { ProductService } from 'src/app/shared/services/product.service';
-import Swal from 'sweetalert2';
+
+import { Product } from 'src/app/shared/models/product.model';
 
 @Component({
   selector: 'app-product-form',
@@ -31,21 +34,31 @@ export class ProductFormComponent implements OnInit {
     private fireStorage: AngularFireStorage,
     private router: Router,
     private route: ActivatedRoute
-    ) {
-    this.categories$ = this.categoryService.getAll();
+    ) {}
 
-    this.productId = this.route.snapshot.paramMap.get('id');
-        if (this.productId) {
-          this.productService.getProductId(this.productId).valueChanges().pipe(take(1)).subscribe(product => {
-          this.product = product;
-        });
-      } else {
-        this.product.date = moment().format('YYYY-MM-DD');
-        this.product.time = moment().format('HH:mm');
-      }
+  ngOnInit() {
+    this.getProductData();
+    this.loadListCategories();
   }
 
-  ngOnInit(): void {
+  getProductData() {
+    this.productId = this.route.snapshot.paramMap.get('id');
+    if (this.productId) {
+      this.productService
+        .getProductId(this.productId)
+        .valueChanges()
+        .pipe(take(1))
+        .subscribe(product => {
+          this.product = product;
+      });
+    } else {
+      this.product.date = moment().format('YYYY-MM-DD');
+      this.product.time = moment().format('HH:mm');
+    }
+  }
+
+  loadListCategories() {
+    this.categories$ = this.categoryService.getAll();
   }
 
   save(product) {
@@ -71,22 +84,21 @@ export class ProductFormComponent implements OnInit {
   async onFileChanged(event) {
     const file = event.target.files[0];
     if (file) {
-       const filePath = `${this.basePath}/${file.name}`;  // path at which image will be stored in the firebase storage
-       this.task =  this.fireStorage.upload(filePath, file);    // upload task
- 
-       // this.progress = this.snapTask.percentageChanges();
-       this.progressValue = this.task.percentageChanges();
- 
-       (await this.task).ref.getDownloadURL().then(url => {this.product.imageUrl = url; });  // <<< url is found here
- 
-     } else {  
-       alert('No images selected');
-       this.product.imageUrl = '';
-      }
-   }
+      const filePath = `${this.basePath}/${file.name}`;  // path at which image will be stored in the firebase storage
+      this.task =  this.fireStorage.upload(filePath, file);    // upload task
 
-   cancel() {
-    this.router.navigate(['/products']);
+      // this.progress = this.snapTask.percentageChanges();
+      this.progressValue = this.task.percentageChanges();
+
+      (await this.task).ref.getDownloadURL().then(url => {this.product.imageUrl = url; });  // <<< url is found here
+
+    } else {  
+      alert('No images selected');
+      this.product.imageUrl = '';
+    }
   }
 
+  cancel() {
+    this.router.navigate(['/products']);
+  }
 }
--- src/app/products/show-product-picture/show-product-picture.component.ts
+++ src/app/products/show-product-picture/show-product-picture.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
-import { Product } from 'src/app/shared/models/product.model';
+
 import { ProductService } from 'src/app/shared/services/product.service';
+
+import { Product } from 'src/app/shared/models/product.model';
 
 @Component({
   selector: 'show-product-picture',
@@ -14,14 +17,19 @@ export class ShowProductPictureComponent implements OnInit {
 
   @Input() product: Product = new Product();
   @Input() isGrid: boolean;
+
   pictureProduct: string;
   basePath = '/PicturesProducts';
   task: AngularFireUploadTask;
   progressValue: Observable<number>;
 
-  constructor(public modalService: NgbModal, private fireStorage: AngularFireStorage, private productService: ProductService) { }
+  constructor(
+    public modalService: NgbModal, 
+    private fireStorage: AngularFireStorage, 
+    private productService: ProductService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   showProductImage(contentProductPicture) {
@@ -32,22 +40,21 @@ export class ShowProductPictureComponent implements OnInit {
   async onFileChanged(event) {
     const file = event.target.files[0];
     if (file) {
-       const filePath = `${this.basePath}/${file.name}`;  // path at which image will be stored in the firebase storage
-       this.task =  this.fireStorage.upload(filePath, file);    // upload task
- 
-       // this.progress = this.snapTask.percentageChanges();
-       this.progressValue = this.task.percentageChanges();
- 
-       (await this.task).ref.getDownloadURL().then(url => {
-         this.pictureProduct = this.product.imageUrl = url;
-         this.productService.update(this.product.key, this.product);
-         this.modalService.dismissAll();
-        });  // <<< url is found here
- 
-     } else {  
-       alert('No images selected');
-       this.pictureProduct = '';
-      }
-   }
+      const filePath = `${this.basePath}/${file.name}`;  // path at which image will be stored in the firebase storage
+      this.task =  this.fireStorage.upload(filePath, file);    // upload task
 
+      // this.progress = this.snapTask.percentageChanges();
+      this.progressValue = this.task.percentageChanges();
+
+      (await this.task).ref.getDownloadURL().then(url => {
+        this.pictureProduct = this.product.imageUrl = url;
+        this.productService.update(this.product.key, this.product);
+        this.modalService.dismissAll();
+      });  // <<< url is found here
+
+    } else {  
+      alert('No images selected');
+      this.pictureProduct = '';
+    }
+   }
 }
