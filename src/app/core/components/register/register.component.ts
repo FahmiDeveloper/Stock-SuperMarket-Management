072b7e625d9c73bb5fcaa5a889bebc5c48521525
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-04-18 14:20:50 +0100

    fix code in register form

--- src/app/core/components/register/register.component.ts
+++ src/app/core/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
 import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
@@ -8,7 +9,8 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+
+export class RegisterComponent implements OnInit{
 
   registerForm: FormGroup;
   errorMessage: string = '';
@@ -18,57 +20,58 @@ export class RegisterComponent {
     public authService: AuthService,
     private router: Router,
     private fb: FormBuilder
-  ) {
-    this.createForm();
-   }
+  ) {}
 
-   createForm() {
-     this.registerForm = this.fb.group({
-       email: ['', Validators.required ],
-       password: ['',Validators.required]
-     });
-   }
+  ngOnInit() {
+    this.validateForm();
+  }
 
-   tryFacebookLogin(){
-     this.authService.doFacebookLogin()
-     .then(res =>{
-      this.onSuccess();
-     }, err => console.log(err)
-     )
-   }
+  validateForm() {
+    this.registerForm = this.fb.group({
+      email: ['', Validators.required ],
+      password: ['',Validators.required]
+    });
+  }
 
-   tryTwitterLogin(){
-     this.authService.doTwitterLogin()
-     .then(res =>{
-      this.onSuccess();
-     }, err => console.log(err)
-     )
-   }
+  tryFacebookLogin(){
+    this.authService.doFacebookLogin()
+    .then(res =>{
+    this.onSuccess();
+    }, err => console.log(err)
+    )
+  }
 
-   tryGoogleLogin(){
-     this.authService.doGoogleLogin()
-     .then(res =>{
-      this.onSuccess();
-     }, err => console.log(err)
-     )
-   }
+  tryTwitterLogin(){
+    this.authService.doTwitterLogin()
+    .then(res =>{
+    this.onSuccess();
+    }, err => console.log(err)
+    )
+  }
 
-   tryRegister(value){
-     this.authService.doRegister(value)
-     .then(res => {
-       console.log(res);
-       this.errorMessage = "";
-       this.successMessage = "Your account has been created";
-     }, err => {
-       console.log(err);
-       this.errorMessage = err.message;
-       this.successMessage = "";
-     })
-   }
+  tryGoogleLogin(){
+    this.authService.doGoogleLogin()
+    .then(res =>{
+    this.onSuccess();
+    }, err => console.log(err)
+    )
+  }
 
-   onSuccess(){
+  tryRegister(value){
+    this.authService.doRegister(value)
+    .then(res => {
+      console.log(res);
+      this.errorMessage = "";
+      this.successMessage = "Your account has been created";
+    }, err => {
+      console.log(err);
+      this.errorMessage = err.message;
+      this.successMessage = "";
+    })
+  }
+
+  onSuccess(){
     this.router.navigate(['/home']);
     this.authService.isConnected.next(true);
   }
-
 }
