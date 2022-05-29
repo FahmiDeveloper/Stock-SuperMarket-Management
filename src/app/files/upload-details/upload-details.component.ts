354790ee2d57b65fa0c068170d1d698e2519e118
FahmiDeveloper <coursangularfahmi@gmail.com>
2022-05-29 11:46:40 +0100

    work part one

--- src/app/files/upload-details/upload-details.component.ts
+++ src/app/files/upload-details/upload-details.component.ts
@@ -1,7 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { from, Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import Swal from 'sweetalert2';
+import * as fileSaver from 'file-saver';
+import * as JSZip from 'jszip/dist/jszip';
 
 import { FileUploadService } from 'src/app/shared/services/file-upload.service';
 
@@ -19,10 +24,11 @@ export class UploadDetailsComponent implements OnInit {
   @Input() isMobile: boolean;
 
   urlFile: string;
-  modalRefPictureFile: any;
-  modalRefOtherFile: any;
   pictureFile: string;
   FileName: string;
+
+  $zipFiles: Observable<ZipFile[]>;
+  isLoading: boolean;
 
   constructor(
     private uploadService: FileUploadService,
@@ -30,6 +36,57 @@ export class UploadDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {}
+
+  viewOtherFileUpload(fileUpload: FileUpload, showFile) {
+    this.urlFile = fileUpload.url;
+    this.FileName = fileUpload.name;
+
+    if (this.isMobile) {
+       this.modalService.open(showFile as Component, { windowClass: 'my-class-mobile', centered: true });
+    } else {
+       this.modalService.open(showFile as Component, { windowClass: 'my-class', centered: true });
+    }
+  }
+
+  viewFilePictureUpload(fileUpload: FileUpload, showPicture) {
+    this.pictureFile = fileUpload.url;
+    this.FileName = fileUpload.name;
+
+    this.modalService.open(showPicture as Component, { size: 'lg', centered: true });
+  }
+
+  showZipFile(fileUpload: FileUpload, viewZipFile) {
+    this.FileName = fileUpload.name;
+    this.isLoading = true;
+
+    fetch(fileUpload.url)
+    .then(res => res.blob()) // Gets the response and returns it as a blob
+    .then(blob => {
+      const zipLoaded = new JSZip.default();
+
+      this.$zipFiles = from(zipLoaded.loadAsync(blob)).pipe(
+        switchMap((zip: any):Observable<ZipFile[]> => {
+          return of(Object.keys(zip.files).map((key)=>zip.files[key]))
+        })
+      )
+      this.$zipFiles.forEach(zipFiles => {
+        zipFiles.forEach(element => {
+          let n = element.name.lastIndexOf('/');
+          element.fileName = element.name.substring(n + 1);
+        })        
+      })
+      this.modalService.open(viewZipFile as Component, { size: 'lg', centered: true });     
+    });
+    setTimeout(() => this.isLoading = false, 3000); 
+  }
+
+  downloadFile(fileUpload: FileUpload) {
+    fetch(fileUpload.url)
+    .then(res => res.blob()) // Gets the response and returns it as a blob
+    .then(blob => {
+      fileSaver.saveAs(blob, fileUpload.name);
+    });
+  }
 
   deleteFileUpload(fileUpload: FileUpload) {
     Swal.fire({
@@ -51,31 +108,25 @@ export class UploadDetailsComponent implements OnInit {
     })
   }
 
-  viewOtherFileUpload(fileUpload: FileUpload, showFile) {
-    if (this.isMobile) {
-      this.modalRefOtherFile = this.modalService.open(showFile as Component, { windowClass: 'my-class-mobile', centered: true });
-    } else {
-      this.modalRefOtherFile = this.modalService.open(showFile as Component, { windowClass: 'my-class', centered: true });
-    }
-    if (fileUpload) {
-      this.urlFile = fileUpload.url;
-      this.FileName = fileUpload.name;
-    }
-  }
-
-  viewFilePictureUpload(fileUpload: FileUpload, showPicture) {
-     this.modalRefPictureFile = this.modalService.open(showPicture, { size: 'lg', centered: true });
-    if (fileUpload) {
-      this.pictureFile = fileUpload.url;
-      this.FileName = fileUpload.name;
-    }
-  }
-
   checkIsImage(path) {
     let imageExtentions = ['JPEG', 'JPG', 'PNG', 'GIF', 'TIFF']; // Array of image extention
     let n = path.lastIndexOf('.');
     let extention: string = path.toLocaleUpperCase().substring(n + 1);
-
     return imageExtentions.indexOf(extention) != -1;
   }
+
+  isZip(path) {
+    let extentionvideo = ['zip']; // Array of video extention
+    let n = path.lastIndexOf('.');
+    let extention: string = path.substring(n + 1);
+    return extentionvideo.indexOf(extention) != -1;
+  }
+}
+
+export interface ZipFile {
+  readonly name: string;
+  readonly dir: boolean;
+  readonly date: Date;
+  readonly data: any;
+  fileName: string;
 }
