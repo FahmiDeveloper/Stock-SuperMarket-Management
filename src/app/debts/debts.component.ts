a06844d2fc0d3fc891dd8d7c3a1451b7ff1542f0
FahmiDeveloper <coursangularfahmi@gmail.com>
2022-06-19 12:06:36 +0100

    fix total for reason type input of financial debt

--- src/app/debts/debts.component.ts
+++ src/app/debts/debts.component.ts
@@ -40,11 +40,17 @@ export class DebtsComponent implements OnInit, OnDestroy {
   restInBox: string = "";
   restInPosteAccount: string = "";
   queryNote: string = "";
-  totalOutDebts: number;
-  totalInDebts: number;
   placeId: number;
-  totalOutDebtsInModal: number;
-  totalInDebtsInModal: number;
+
+  
+  //in debt attributes
+  totalInDebts: string = "";
+  defaultTotalInDebts: number;
+  customTotalInDebts: number;
+
+  totalInDebtsInModal: string = "";
+  defaultTotalInDebtsInModal: number;
+  customTotalInDebtsInModal: number;
 
   toPayThisMonth: boolean = true;
   toPayNextMonth: boolean = false;
@@ -52,6 +58,15 @@ export class DebtsComponent implements OnInit, OnDestroy {
   checkToPayThisMonth: boolean = false;
   checkToPayNextMonth: boolean = false;
   checkNotToPay: boolean = false;
+
+   //out debt attributes
+  totalOutDebts: string = "";
+  defaultTotalOutDebts: number;
+  customTotalOutDebts: number;
+
+  totalOutDebtsInModal: string = "";
+  defaultTotalOutDebtsInModal: number;
+  customTotalOutDebtsInModal: number;
 
   toGetThisMonth: boolean = true;
   toGetNextMonth: boolean = false;
@@ -205,17 +220,67 @@ export class DebtsComponent implements OnInit, OnDestroy {
   }
 
   getTotalOutDebts() {
-    this.totalOutDebts = 0;
+    this.defaultTotalOutDebts = 0;
+    this.customTotalOutDebts = 0;
+
     this.filteredDebtsCopie.filter(debt => debt.creditor == "Fahmi").forEach(element => {
-      this.totalOutDebts += Number(element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT")));
+      if (element.financialDebt.indexOf("DT") !== -1) {
+        element.financialOutDebtWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT"));
+        element.financialOutDebtWithConvert = element.financialOutDebtWithConvert + '000';
+      }
+      if (element.financialDebt.indexOf("Mill") !== -1) {
+        element.financialOutDebtWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("Mill"));
+      }
+
+      this.defaultTotalOutDebts += Number(element.financialOutDebtWithConvert);
+
+      if (this.defaultTotalOutDebts.toString().length > 4) {
+
+        this.customTotalOutDebts = this.defaultTotalOutDebts / 1000;
+        if (String(this.customTotalOutDebts).includes(".")){
+          const customTotalOutDebts = String(this.customTotalOutDebts).split('.');
+          if (customTotalOutDebts[1].length == 1) this.totalOutDebts = customTotalOutDebts[0] + "DT." + customTotalOutDebts[1] + "00Mill";
+          else if (customTotalOutDebts[1].length == 2) this.totalOutDebts = customTotalOutDebts[0] + "DT." + customTotalOutDebts[1] + "0Mill";
+          else this.totalOutDebts = customTotalOutDebts[0] + "DT." + customTotalOutDebts[1] + "Mill";
+        } else {
+          this.totalOutDebts = String(this.customTotalOutDebts) + "DT";
+        }
+      } else {
+        this.totalOutDebts = this.defaultTotalOutDebts + "Mill";
+      }
     });
   }
 
   getTotalIntDebts() {
-    this.totalInDebts = 0;
+    this.defaultTotalInDebts = 0;
+    this.customTotalInDebts = 0;
+
     this.filteredDebtsCopie.filter(debt => debt.debtor == "Fahmi").forEach(element => {
-      this.totalInDebts += Number(element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT")));
-    });  
+      if (element.financialDebt.indexOf("DT") !== -1) {
+        element.financialInDebtWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT"));
+        element.financialInDebtWithConvert = element.financialInDebtWithConvert + '000';
+      }
+      if (element.financialDebt.indexOf("Mill") !== -1) {
+        element.financialInDebtWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("Mill"));
+      }
+
+      this.defaultTotalInDebts += Number(element.financialInDebtWithConvert);
+      if (this.defaultTotalInDebts.toString().length > 4) {
+
+        this.customTotalInDebts = this.defaultTotalInDebts / 1000;
+
+        if (String(this.customTotalInDebts).includes(".")){
+          const customTotalInDebts = String(this.customTotalInDebts).split('.');
+          if (customTotalInDebts[1].length == 1) this.totalInDebts = customTotalInDebts[0] + "DT." + customTotalInDebts[1] + "00Mill";
+          else if (customTotalInDebts[1].length == 2) this.totalInDebts = customTotalInDebts[0] + "DT." + customTotalInDebts[1] + "0Mill";
+          else this.totalInDebts = customTotalInDebts[0] + "DT." + customTotalInDebts[1] + "Mill";
+        } else {
+          this.totalInDebts = String(this.customTotalInDebts) + "DT";
+        }
+      } else {
+        this.totalInDebts = this.defaultTotalInDebts + "Mill";
+      }
+    }); 
   }
 
   showDetInDebt(contentDetInDebt) {
@@ -229,7 +294,9 @@ export class DebtsComponent implements OnInit, OnDestroy {
   }
 
   payThisMonth() {
-    this.totalInDebtsInModal = 0;
+    this.totalInDebtsInModal = "";
+    this.defaultTotalInDebtsInModal = 0;
+    this.customTotalInDebtsInModal = 0;
 
     if (this.toPayThisMonth) {
       this.toPayNextMonth= false;
@@ -237,12 +304,37 @@ export class DebtsComponent implements OnInit, OnDestroy {
       this.detailsInDebt = this.filteredDebtsCopie.filter(debt => (debt.debtor == "Fahmi") && (debt.toPayThisMonth == true));
     }
     this.detailsInDebt.forEach(element => {
-      this.totalInDebtsInModal += Number(element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT")));
+      if (element.financialDebt.indexOf("DT") !== -1) {
+        element.financialInDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT"));
+        element.financialInDebtInModalWithConvert = element.financialInDebtInModalWithConvert + '000';
+      }
+      if (element.financialDebt.indexOf("Mill") !== -1) {
+        element.financialInDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("Mill"));
+      }
+
+      this.defaultTotalInDebtsInModal += Number(element.financialInDebtInModalWithConvert);
+      if (this.defaultTotalInDebtsInModal.toString().length > 4) {
+
+        this.customTotalInDebtsInModal = this.defaultTotalInDebtsInModal / 1000;
+
+        if (String(this.customTotalInDebtsInModal).includes(".")){
+          const customTotalInDebtsInModal = String(this.customTotalInDebtsInModal).split('.');
+          if (customTotalInDebtsInModal[1].length == 1) this.totalInDebtsInModal = customTotalInDebtsInModal[0] + "DT." + customTotalInDebtsInModal[1] + "00Mill";
+          else if (customTotalInDebtsInModal[1].length == 2) this.totalInDebtsInModal = customTotalInDebtsInModal[0] + "DT." + customTotalInDebtsInModal[1] + "0Mill";
+          else this.totalInDebtsInModal = customTotalInDebtsInModal[0] + "DT." + customTotalInDebtsInModal[1] + "Mill";
+        } else {
+          this.totalInDebtsInModal = String(this.customTotalInDebtsInModal) + "DT";
+        }
+      } else {
+        this.totalInDebtsInModal = this.defaultTotalInDebtsInModal + "Mill";
+      }
     });
   }
 
   payNextMonth() {
-    this.totalInDebtsInModal = 0;
+    this.totalInDebtsInModal = "";
+    this.defaultTotalInDebtsInModal = 0;
+    this.customTotalInDebtsInModal = 0;
 
     if (this.toPayNextMonth) {
       this.toPayThisMonth = false;
@@ -250,12 +342,37 @@ export class DebtsComponent implements OnInit, OnDestroy {
       this.detailsInDebt = this.filteredDebtsCopie.filter(debt => (debt.debtor == "Fahmi") && (debt.toPayNextMonth == true));
     }
     this.detailsInDebt.forEach(element => {
-      this.totalInDebtsInModal += Number(element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT")));
+      if (element.financialDebt.indexOf("DT") !== -1) {
+        element.financialInDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT"));
+        element.financialInDebtInModalWithConvert = element.financialInDebtInModalWithConvert + '000';
+      }
+      if (element.financialDebt.indexOf("Mill") !== -1) {
+        element.financialInDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("Mill"));
+      }
+
+      this.defaultTotalInDebtsInModal += Number(element.financialInDebtInModalWithConvert);
+      if (this.defaultTotalInDebtsInModal.toString().length > 4) {
+
+        this.customTotalInDebtsInModal = this.defaultTotalInDebtsInModal / 1000;
+
+        if (String(this.customTotalInDebtsInModal).includes(".")){
+          const customTotalInDebtsInModal = String(this.customTotalInDebtsInModal).split('.');
+          if (customTotalInDebtsInModal[1].length == 1) this.totalInDebtsInModal = customTotalInDebtsInModal[0] + "DT." + customTotalInDebtsInModal[1] + "00Mill";
+          else if (customTotalInDebtsInModal[1].length == 2) this.totalInDebtsInModal = customTotalInDebtsInModal[0] + "DT." + customTotalInDebtsInModal[1] + "0Mill";
+          else this.totalInDebtsInModal = customTotalInDebtsInModal[0] + "DT." + customTotalInDebtsInModal[1] + "Mill";
+        } else {
+          this.totalInDebtsInModal = String(this.customTotalInDebtsInModal) + "DT";
+        }
+      } else {
+        this.totalInDebtsInModal = this.defaultTotalInDebtsInModal + "Mill";
+      }
     });
   }
 
   notToBePay() {
-    this.totalInDebtsInModal = 0;
+    this.totalInDebtsInModal = "";
+    this.defaultTotalInDebtsInModal = 0;
+    this.customTotalInDebtsInModal = 0;
 
     if (this.notToPay) {
       this.toPayThisMonth = false;
@@ -263,12 +380,37 @@ export class DebtsComponent implements OnInit, OnDestroy {
       this.detailsInDebt = this.filteredDebtsCopie.filter(debt => (debt.debtor == "Fahmi") && (debt.notToPayForNow == true));
     }
     this.detailsInDebt.forEach(element => {
-      this.totalInDebtsInModal += Number(element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT")));
+      if (element.financialDebt.indexOf("DT") !== -1) {
+        element.financialInDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT"));
+        element.financialInDebtInModalWithConvert = element.financialInDebtInModalWithConvert + '000';
+      }
+      if (element.financialDebt.indexOf("Mill") !== -1) {
+        element.financialInDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("Mill"));
+      }
+
+      this.defaultTotalInDebtsInModal += Number(element.financialInDebtInModalWithConvert);
+      if (this.defaultTotalInDebtsInModal.toString().length > 4) {
+
+        this.customTotalInDebtsInModal = this.defaultTotalInDebtsInModal / 1000;
+
+        if (String(this.customTotalInDebtsInModal).includes(".")){
+          const customTotalInDebtsInModal = String(this.customTotalInDebtsInModal).split('.');
+          if (customTotalInDebtsInModal[1].length == 1) this.totalInDebtsInModal = customTotalInDebtsInModal[0] + "DT." + customTotalInDebtsInModal[1] + "00Mill";
+          else if (customTotalInDebtsInModal[1].length == 2) this.totalInDebtsInModal = customTotalInDebtsInModal[0] + "DT." + customTotalInDebtsInModal[1] + "0Mill";
+          else this.totalInDebtsInModal = customTotalInDebtsInModal[0] + "DT." + customTotalInDebtsInModal[1] + "Mill";
+        } else {
+          this.totalInDebtsInModal = String(this.customTotalInDebtsInModal) + "DT";
+        }
+      } else {
+        this.totalInDebtsInModal = this.defaultTotalInDebtsInModal + "Mill";
+      }
     });
   }
 
   getThisMonth() {
-    this.totalOutDebtsInModal = 0;
+    this.totalOutDebtsInModal = "";
+    this.defaultTotalOutDebtsInModal = 0;
+    this.customTotalOutDebtsInModal = 0;
 
     if (this.toGetThisMonth) {
       this.toGetNextMonth = false;
@@ -276,12 +418,37 @@ export class DebtsComponent implements OnInit, OnDestroy {
       this.detailsOutDebt = this.filteredDebtsCopie.filter(debt => (debt.creditor == "Fahmi") && (debt.toGetThisMonth == true));
     }
     this.detailsOutDebt.forEach(element => {
-      this.totalOutDebtsInModal += Number(element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT")));
+      if (element.financialDebt.indexOf("DT") !== -1) {
+        element.financialOutDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT"));
+        element.financialOutDebtInModalWithConvert = element.financialOutDebtInModalWithConvert + '000';
+      }
+      if (element.financialDebt.indexOf("Mill") !== -1) {
+        element.financialOutDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("Mill"));
+      }
+
+      this.defaultTotalOutDebtsInModal += Number(element.financialOutDebtInModalWithConvert);
+      if (this.defaultTotalOutDebtsInModal.toString().length > 4) {
+
+        this.customTotalOutDebtsInModal = this.defaultTotalOutDebtsInModal / 1000;
+
+        if (String(this.customTotalOutDebtsInModal).includes(".")){
+          const customTotalOutDebtsInModal = String(this.customTotalOutDebtsInModal).split('.');
+          if (customTotalOutDebtsInModal[1].length == 1) this.totalOutDebtsInModal = customTotalOutDebtsInModal[0] + "DT." + customTotalOutDebtsInModal[1] + "00Mill";
+          else if (customTotalOutDebtsInModal[1].length == 2) this.totalOutDebtsInModal = customTotalOutDebtsInModal[0] + "DT." + customTotalOutDebtsInModal[1] + "0Mill";
+          else this.totalOutDebtsInModal = customTotalOutDebtsInModal[0] + "DT." + customTotalOutDebtsInModal[1] + "Mill";
+        } else {
+          this.totalOutDebtsInModal = String(this.customTotalOutDebtsInModal) + "DT";
+        }
+      } else {
+        this.totalOutDebtsInModal = this.defaultTotalOutDebtsInModal + "Mill";
+      }
     });
   }
 
   getNextMonth() {
-    this.totalOutDebtsInModal = 0;
+    this.totalOutDebtsInModal = "";
+    this.defaultTotalOutDebtsInModal = 0;
+    this.customTotalOutDebtsInModal = 0;
 
     if (this.toGetNextMonth) {
       this.toGetThisMonth = false;
@@ -289,12 +456,37 @@ export class DebtsComponent implements OnInit, OnDestroy {
       this.detailsOutDebt = this.filteredDebtsCopie.filter(debt => (debt.creditor == "Fahmi") && (debt.toGetNextMonth == true));
     }
     this.detailsOutDebt.forEach(element => {
-      this.totalOutDebtsInModal += Number(element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT")));
+      if (element.financialDebt.indexOf("DT") !== -1) {
+        element.financialOutDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT"));
+        element.financialOutDebtInModalWithConvert = element.financialOutDebtInModalWithConvert + '000';
+      }
+      if (element.financialDebt.indexOf("Mill") !== -1) {
+        element.financialOutDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("Mill"));
+      }
+
+      this.defaultTotalOutDebtsInModal += Number(element.financialOutDebtInModalWithConvert);
+      if (this.defaultTotalOutDebtsInModal.toString().length > 4) {
+
+        this.customTotalOutDebtsInModal = this.defaultTotalOutDebtsInModal / 1000;
+
+        if (String(this.customTotalOutDebtsInModal).includes(".")){
+          const customTotalOutDebtsInModal = String(this.customTotalOutDebtsInModal).split('.');
+          if (customTotalOutDebtsInModal[1].length == 1) this.totalOutDebtsInModal = customTotalOutDebtsInModal[0] + "DT." + customTotalOutDebtsInModal[1] + "00Mill";
+          else if (customTotalOutDebtsInModal[1].length == 2) this.totalOutDebtsInModal = customTotalOutDebtsInModal[0] + "DT." + customTotalOutDebtsInModal[1] + "0Mill";
+          else this.totalOutDebtsInModal = customTotalOutDebtsInModal[0] + "DT." + customTotalOutDebtsInModal[1] + "Mill";
+        } else {
+          this.totalOutDebtsInModal = String(this.customTotalOutDebtsInModal) + "DT";
+        }
+      } else {
+        this.totalOutDebtsInModal = this.defaultTotalOutDebtsInModal + "Mill";
+      }
     });
   }
 
   notToBeGet() {
-    this.totalOutDebtsInModal = 0;
+    this.totalOutDebtsInModal = "";
+    this.defaultTotalOutDebtsInModal = 0;
+    this.customTotalOutDebtsInModal = 0;
 
     if (this.notToGet) {
       this.toGetThisMonth = false;
@@ -302,7 +494,30 @@ export class DebtsComponent implements OnInit, OnDestroy {
       this.detailsOutDebt = this.filteredDebtsCopie.filter(debt => (debt.creditor == "Fahmi") && (debt.notToGetForNow == true));
     }
     this.detailsOutDebt.forEach(element => {
-      this.totalOutDebtsInModal += Number(element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT")));
+      if (element.financialDebt.indexOf("DT") !== -1) {
+        element.financialOutDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("DT"));
+        element.financialOutDebtInModalWithConvert = element.financialOutDebtInModalWithConvert + '000';
+      }
+      if (element.financialDebt.indexOf("Mill") !== -1) {
+        element.financialOutDebtInModalWithConvert = element.financialDebt.substring(0, element.financialDebt.lastIndexOf("Mill"));
+      }
+
+      this.defaultTotalOutDebtsInModal += Number(element.financialOutDebtInModalWithConvert);
+      if (this.defaultTotalOutDebtsInModal.toString().length > 4) {
+
+        this.customTotalOutDebtsInModal = this.defaultTotalOutDebtsInModal / 1000;
+
+        if (String(this.customTotalOutDebtsInModal).includes(".")){
+          const customTotalOutDebtsInModal = String(this.customTotalOutDebtsInModal).split('.');
+          if (customTotalOutDebtsInModal[1].length == 1) this.totalOutDebtsInModal = customTotalOutDebtsInModal[0] + "DT." + customTotalOutDebtsInModal[1] + "00Mill";
+          else if (customTotalOutDebtsInModal[1].length == 2) this.totalOutDebtsInModal = customTotalOutDebtsInModal[0] + "DT." + customTotalOutDebtsInModal[1] + "0Mill";
+          else this.totalOutDebtsInModal = customTotalOutDebtsInModal[0] + "DT." + customTotalOutDebtsInModal[1] + "Mill";
+        } else {
+          this.totalOutDebtsInModal = String(this.customTotalOutDebtsInModal) + "DT";
+        }
+      } else {
+        this.totalOutDebtsInModal = this.defaultTotalOutDebtsInModal + "Mill";
+      }
     });
   }
 
