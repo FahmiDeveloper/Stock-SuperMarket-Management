576bb70fd6375109e04660fd872ece51603c39b8
FahmiDeveloper <71970237+FahmiDeveloper@users.noreply.github.com>
2023-07-09 12:53:16 +0200

    part two

--- src/app/clockings/for-mobile/clocking-form-mobile/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './clocking-form-mobile.component';
--- src/app/medications/show-medication-details/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './show-medication-details.component';
-export * from './show-medication-details.component.animations';
